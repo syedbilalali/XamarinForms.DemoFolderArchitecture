10159da4fc70d252fe6cd73902b2f17a7108dadf
syedbilalali <syedbilalali.dev@gmail.com>
2022-01-06 01:58:54 +0530

    add some folder and cs files for project enviornment

---
+++ Sample.FolderArchitecture/Sample.FolderArchitecture/Sample.FolderArchitecture/APIServices/APIService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.FolderArchitecture.APIServices
+{
+    internal class APIService
+    {
+    }
+}
---
+++ Sample.FolderArchitecture/Sample.FolderArchitecture/Sample.FolderArchitecture/Helper/ConstantFields.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.FolderArchitecture.Helper
+{
+    internal class ConstantFields
+    {
+    }
+}
